54d48f42b738b61872ef7914b702c19e5806669f
Kelsie Braidwood <kelsieiona@gmail.com>
2018-04-07 15:10:16 +0100

    customer has money passing

--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -6,11 +6,14 @@ require_relative("../guest.rb")
 
 class TestGuest < MiniTest::Test
   def setup()
-    @guest = Guest.new("Kelsie")
+    @guest = Guest.new("Kelsie", 50.0)
   end
 
   def test_guest_has_name()
     assert_equal("Kelsie", @guest.name)
+  end
+  def test_guest_has_money()
+    assert_equal(50.0, @guest.money)
   end
 
 end
